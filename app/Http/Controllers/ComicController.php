549e189c64140e98782336f162907c7f23a5b94c
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-03-08 15:57:35 +0100

    Migration

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class BranchController extends Controller
+class ComicController extends Controller
 {
     /**
      * Display a listing of the resource.
---
+++ database/migrations/2022_03_08_143934_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 100);
+            $table->string('thumb', 100);
+            $table->float('price', 8, 2);
+            $table->string('series', 100);
+            $table->text('description');
+            $table->date('sale_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
