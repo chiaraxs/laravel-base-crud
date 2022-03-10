fbdcfa333a3b6ca3e2b696785c18f39d4819d046
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-03-10 15:34:36 +0100

    Added: old() in form value

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -11,6 +11,7 @@
 {{-- il method post mi permette la creazione del nuovo comic --}}
 {{-- i methods put e patch, invece, aggiornano il form --}}
 {{-- validation -> se la validazione fallisce -> rimanda al form con gli errori da sistemare --}}
+{{-- la funzione old() recupera il dato inviato all'array precedentemente, lo rende visibile dall'utente e modificabile --}}
 @section('content')
 
 
@@ -31,38 +32,38 @@
 
             <div class="mb-3">
                 <label for="title" class="form-label me-2">Title</label>
-                <input type="text" name="title" placeholder="Insert Title">
+                <input type="text" name="title" placeholder="Insert Title" value="{{old('title')}}">
             </div>
 
             <div class="mb-3">
                 <label for="thumb" class="form-label me-2">Thumb</label>
-                <input type="text" name="thumb" placeholder="Insert Thumb">
+                <input type="text" name="thumb" placeholder="Insert Thumb" value="{{old('thumb')}}">
             </div>
 
 
             <div class="mb-3">
                 <label for="price" class="form-label me-2">Price</label>
-                <input type="text" name="price" placeholder="Insert Price">
+                <input type="text" name="price" placeholder="Insert Price" value="{{old('price')}}">
             </div>
 
             <div class="mb-3">
                 <label for="series" class="form-label me-2">Series</label>
-                <input type="text" name="series" placeholder="Insert Series">
+                <input type="text" name="series" placeholder="Insert Series" value="{{old('series')}}">
             </div>
 
             <div class="mb-3">
                 <label for="sale_date" class="form-label me-2">Sale Date</label>
-                <input type="text" name="sale_date" placeholder="Insert Sale Date">
+                <input type="text" name="sale_date" placeholder="Insert Sale Date" value="{{old('sale_date')}}">
             </div>
 
             <div class="mb-3">
                 <label for="type" class="form-label me-2">Type</label>
-                <input type="text" name="type" placeholder="Insert Type">
+                <input type="text" name="type" placeholder="Insert Type" value="{{old('type')}}">
             </div>
 
             <div class="mb-3">
                 <label for="description" class="form-label me-2">Description</label>
-                <textarea class="form-control" name="description" placeholder="Insert Description" rows="3"></textarea>
+                <textarea class="form-control" name="description" placeholder="Insert Description" rows="3" value="{{old('description')}}"></textarea>
             </div>
 
             <input type="submit" class="btn btn-info" value="Create comic"></button>
